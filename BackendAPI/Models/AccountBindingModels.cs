986629e723e72b3ea19e5bb8251c78661da3f70e
Binod Marikhu <bnod_468@hotmail.com>
2017-08-05 06:59:20 +0545

    Application User Edited

--- BackendAPI/Models/AccountBindingModels.cs
+++ BackendAPI/Models/AccountBindingModels.cs
@@ -39,6 +39,11 @@ namespace BackendAPI.Models
         public string Email { get; set; }
 
         [Required]
+        [Display(Name = "Phone")]
+        [StringLength(100, ErrorMessage = "Phone Number should be of length 10")]
+        public string Phone { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
